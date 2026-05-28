446965ac11405ae0400ef8fc37d38913746ec791
anand-lab25 <anand.business1988@gmail.com>
2026-05-28 08:55:33 +0000

    feat: add a metamask button

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,23 @@
+"use client";
+import {useEffect,useState} from "react";
+import {ethers} from "ethers";
 export default function Home() {
-  return (
-    <h1>Hello James, this is my homepage!</h1>
-  );
+    const[account, setAccount]= useState(null);
+    async function connectWallet(){
+        if(window.ethereum) {
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            const accounts = await provider.send("eth_requestAccounts",[]);
+            setAccount(accounts[0]);
+
+        } else {
+            alert("Please install Metamask!");
+        }
+    }
+    return(
+        <main>
+            <h1>Welcome to My First Dapp</h1>
+            <button onClick={connectWallet}>ConnectWallet</button>
+            {account && <p>connected: {account}</p> }
+        </main>
+    )
 }
